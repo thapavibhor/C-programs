cf47162555c1ded6b2002dd95f6002077a2222c0
thapavibhor <thapavibhorv7@gmail.com>
2025-09-25 23:07:55 +0530

    Add files via upload

---
+++ EVEN_AND.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+ int x;
+ printf("Enter a number :");
+ scanf("%d",&x);
+ if (x%2==0)
+ {
+  printf("\nEven");
+ }
+ else
+ {
+  printf("\nOdd");
+ }
+  getch();
+  clrscr();
+}
